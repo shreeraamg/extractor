1eef67ab5266e39ff63d7ce4ff187f480d754f4b
Shreeraam G <shreeram0312@gmail.com>
2025-10-02 08:57:37 +0000

    Openrewrite organize imports

--- src/test/java/com/example/extractor/ExtractorApplicationTests.java
+++ src/test/java/com/example/extractor/ExtractorApplicationTests.java
@@ -21,8 +21,8 @@ import static org.mockito.Mockito.any;
 import static org.mockito.Mockito.anyString;
 import static org.mockito.Mockito.eq;
 import static org.mockito.Mockito.never;
-import static org.mockito.Mockito.when;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 class ExtractorApplicationTests {
 
@@ -34,6 +34,7 @@ class ExtractorApplicationTests {
 
     private Validator validator;
 
+    @InjectMocks
     private ProductMessageHandler handler;
 
     @BeforeEach
